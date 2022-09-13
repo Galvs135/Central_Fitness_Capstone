1c9592394ef729530dcfef5523cc58f66496bc4e
ale <ale@ale.com>
2022-09-13 20:07:25 -0300

    feat: created list of the training

--- src/Pages/Training/index.tsx
+++ src/Pages/Training/index.tsx
@@ -1,3 +1,48 @@
+import { Box, Flex, Image } from "@chakra-ui/react";
+import { useContext } from "react";
+import { CardTraining } from "../../Components/CardTraining";
+import { MuscleContext } from "../../Providers/Muscle";
+
+import TrainingImage from "../../Imgs/training.png";
+import { theme } from "../../Styles/theme";
+import { useLogin } from "../../Providers/Login";
+import { useEffect } from "react";
+
 export const Training = () => {
-  return <div>Pagina criada para renderizar componente nas rotas (Treino)</div>;
+  const { listTrainigs, loadTraining } = useContext(MuscleContext);
+  const { accessToken, user } = useLogin();
+
+  console.log(user);
+
+  useEffect(() => {
+    if (accessToken) {
+      loadTraining(accessToken, user);
+    }
+  }, []);
+
+  return (
+    <Flex position="relative" w="100%" m="auto">
+      <Flex
+        flexDir="column"
+        alignItems="center"
+        w="100%"
+        h="600px"
+        overflowY="scroll"
+      >
+        {listTrainigs.map((training) => (
+          <CardTraining key={training.id} training={training} />
+        ))}
+      </Flex>
+
+      <Image
+        src={TrainingImage}
+        display={["none", "none", "none", "none", "block"]}
+        position="absolute"
+        left="52%"
+        top="20%"
+        zIndex="-1"
+        w="655px"
+      />
+    </Flex>
+  );
 };
--- src/Providers/Muscle/index.tsx
+++ src/Providers/Muscle/index.tsx
@@ -12,20 +12,39 @@ interface ChildrenProp {
   children: ReactNode;
 }
 
+interface Training {
+  id: number;
+  title: string;
+  category: string;
+  description: string;
+  videoURL: string;
+  genre: "feminino" | "masculino";
+}
+
+interface User {
+  email: string;
+  name: string;
+  genre: string;
+  id: string;
+}
+
 interface MuscleContextData {
   weight: number;
   height: number;
   Muscle: (id: string) => void;
+  listTrainigs: Training[];
+  loadTraining(token: string, user: User): void;
 }
 
 const MuscleContext = createContext<MuscleContextData>({} as MuscleContextData);
 
 const MuscleProvider = ({ children }: ChildrenProp) => {
   const [token, setToken] = useState(
-    localStorage.getItem("Fitnes:accessToken") || ""
+    localStorage.getItem("@Fitness:accessToken") || ""
   );
   const [weight, setWeight] = useState<number>(0);
   const [height, setHeight] = useState<number>(0);
+  const [listTrainigs, setListTrainings] = useState<Training[]>([]);
 
   const Muscle = (id: string) => {
     api
@@ -41,8 +60,32 @@ const MuscleProvider = ({ children }: ChildrenProp) => {
       });
   };
 
+  const loadTraining = (token: string, user: User) => {
+    api
+      .get("/training", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        const list = response.data;
+
+        const filteredByGenre = list.filter(
+          (training: Training) =>
+            training.genre.toLowerCase() === user.genre.toLowerCase()
+        );
+
+        setListTrainings(filteredByGenre);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
-    <MuscleContext.Provider value={{ Muscle, weight, height }}>
+    <MuscleContext.Provider
+      value={{ Muscle, weight, height, listTrainigs, loadTraining }}
+    >
       {children}
     </MuscleContext.Provider>
   );
