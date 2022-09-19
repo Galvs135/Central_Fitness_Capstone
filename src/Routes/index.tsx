6522ae459f4b2d1918203688788015918ffa9f1c
Galvs135 <lg_franca@hotmail.com>
2022-09-19 20:48:26 -0300

    Page: Created page aboutUs.

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,4 +1,5 @@
 import { Redirect, Switch } from "react-router-dom";
+import { AboutUs } from "../Pages/AboutUs";
 import { Exercise } from "../Pages/Exercise";
 import { FitnessHome } from "../Pages/Fitness";
 import { Login } from "../Pages/Login";
@@ -17,6 +18,7 @@ export const RoutesApplication = () => {
       <Route component={Nutrition} path="/nutrition" isPrivate />
       <Route component={Training} exact path="/training" isPrivate />
       <Route component={Exercise} path="/training/:exerciseName" isPrivate />
+      <Route component={AboutUs} path="/aboutUs" isPrivate />
       <Route component={() => <Redirect to="/" />} path="/*" />
     </Switch>
   );
