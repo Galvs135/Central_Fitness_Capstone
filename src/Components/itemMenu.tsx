83e0319bd09a389cd5f25f1bf74e7a44956aad0b
ale <ale@ale.com>
2022-09-21 19:51:49 -0300

    fix: add icon by props

--- src/Components/itemMenu.tsx
+++ src/Components/itemMenu.tsx
@@ -1,5 +1,8 @@
 import { Box, ListItem, Link } from "@chakra-ui/react";
 import { RiHomeSmileFill } from "react-icons/ri";
+import { GiMuscleUp } from "react-icons/gi";
+import { SiMicrosoftteams } from "react-icons/si";
+import { BiFoodMenu } from "react-icons/bi";
 
 import { motion } from "framer-motion";
 import { itemLeft } from "../Styles/animate";
@@ -10,14 +13,23 @@ import linkCss from "../Styles/link";
 interface ItemMenuProps {
   title: string;
   path: string;
+  icon?: string;
 }
 
-export const ItemMenu = ({ title, path }: ItemMenuProps) => {
+export const ItemMenu = ({ title, path, icon }: ItemMenuProps) => {
   return (
     <ListItem as={motion.li} variants={itemLeft}>
       <Link as={RouterLink} to={path} sx={linkCss}>
         <Box as="span" fontSize="1.5rem">
-          <RiHomeSmileFill />
+          {icon === "home" ? (
+            <RiHomeSmileFill />
+          ) : icon === "training" ? (
+            <GiMuscleUp />
+          ) : icon === "team" ? (
+            <SiMicrosoftteams />
+          ) : (
+            <BiFoodMenu />
+          )}
         </Box>
         <span>{title}</span>
       </Link>
